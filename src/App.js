ddfdc90312581b44d08127dc003192f5ee47fc59
Yongho Kim <kyh980909@gmail.com>
2022-02-12 01:16:22 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  
+  let [title, titleChange] = useState(['남자 코트 추천', '강남 우동 맛집', '배고파']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      {
+        title.map((a) => {
+          return (<div className="list">
+        <h3> {a} </h3>
+        <p>2월 12일 발행</p>
+        <hr/>
+      </div>)
+        })
+      }
     </div>
   );
 }
